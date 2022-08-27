cb6c7739752879968f172fca0f9c19246e6ca03f
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2022-08-27 22:12:42 +0530

    Update PROJECT.js

--- PROJECT.js
+++ PROJECT.js
@@ -1,1 +1,8 @@
 save code here
+
+--starting on terminal--
+->npm init 
+package.json is formed 
+-->npm i express 
+package-lock.json is formed : it has detailed explanation about packages we have installed
+express is used in routing 
