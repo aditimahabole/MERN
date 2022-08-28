0b3cc08679e1d0f94db24c55305e13d22b6f3d66
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2022-08-28 11:54:23 +0530

    Update PROJECT.js

--- PROJECT.js
+++ PROJECT.js
@@ -3,6 +3,12 @@ save code here
 --starting on terminal--
 ->npm init 
 package.json is formed 
+
 -->npm i express 
 package-lock.json is formed : it has detailed explanation about packages we have installed
 express is used in routing 
+ thru exprss server 3000 is created by us 
+ 
+ ->now we will use middle ware 
+ basically when user is not authenticated then he/she will not be able to acess the other page
+ for that they have to login first 
