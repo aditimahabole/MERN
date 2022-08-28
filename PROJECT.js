26836c3961b297db65450858be8e063be179cf0c
ADITI MAHABOLE <78752342+aditimahabole@users.noreply.github.com>
2022-08-28 13:26:31 +0530

    Update PROJECT.js

--- PROJECT.js
+++ PROJECT.js
@@ -9,6 +9,11 @@ package-lock.json is formed : it has detailed explanation about packages we have
 express is used in routing 
  thru exprss server 3000 is created by us 
  
- ->now we will use middle ware 
+ -->now we will use middle ware 
  basically when user is not authenticated then he/she will not be able to acess the other page
  for that they have to login first 
+ 
+ --> Now we have to connect mongoDB 
+ for which we have to create cluster and all that stuff in mongoDB atlas
+ 
+ -->install dotenv package
